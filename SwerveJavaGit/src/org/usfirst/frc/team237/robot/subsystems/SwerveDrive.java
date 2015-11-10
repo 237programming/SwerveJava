687ed9a819770fe0aa329bbcd2c8066e74bfec73
237programming <237programming@gmail.com>
2015-11-09 19:48:57 -0500

    Added SwerveDrive.java Added SwerveWheel.java

---
+++ SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team237.robot.subsystems;
+import org.usfirst.frc.team237.robot.math.Vector;
+public class SwerveDrive 
+{
+	SwerveWheel[] wheels = new SwerveWheel[4];
+	Vector position;
+	double rotation;
+	//Init 
+	public void initSwerveDrive()
+	{
+		
+	}
+}
---
+++ SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveWheel.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team237.robot.subsystems;
+import org.usfirst.frc.team237.robot.math.Vector;
+
+public class SwerveWheel 
+{
+	Vector position;
+	double rotationSetpoint;
+	double rotation;
+	double speed;
+	int direction;
+}
