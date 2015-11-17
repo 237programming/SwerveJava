1b240196092956fc9258790b53dd9c548a4de50c
237programming <237programming@gmail.com>
2015-11-16 19:56:13 -0500

    added swerb init and update wheel

--- SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveDrive.java
+++ SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveDrive.java
@@ -2,12 +2,37 @@ package org.usfirst.frc.team237.robot.subsystems;
 import org.usfirst.frc.team237.robot.math.Vector;
 public class SwerveDrive 
 {
-	SwerveWheel[] wheels = new SwerveWheel[4];
+	SwerveWheel[] wheels;
 	Vector position;
 	double rotation;
 	//Init 
 	public void initSwerveDrive()
 	{
+		this.position = new Vector(0.0,0.0);
+		this.rotation = 0.0;
 		
+		this.wheels = new SwerveWheel[4];
+		double scaleX = -1.0;
+		double scaleY = 1.0;
+		
+		for (int i = 0; i < 4; i++ ){
+			if (i == 0 || i == 2)
+			{
+				scaleX = -1.0;
+			}
+			else {
+				scaleX = 1.0;
+			} 
+			if (i == 2 || i == 3)
+			{
+				scaleY = 1.0;
+			}
+			else {
+				scaleY = -1.0;
+			}
+			this.wheels[i].direction = 1;
+			this.wheels[i].rotation = 0.0;
+			this.wheels[i].position = new Vector(scaleX*3.0 / 2.0, scaleY*3.0 / 2.0);
+		}
 	}
 }
--- SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveWheel.java
+++ SwerveJavaGit/src/org/usfirst/frc/team237/robot/subsystems/SwerveWheel.java
@@ -8,4 +8,22 @@ public class SwerveWheel
 	double rotation;
 	double speed;
 	int direction;
+	
+	public void updateWheel(double dt)
+	{
+		/* Clamps the wheel speed between 1.0 and -1.0 */
+		if (this.speed > 1.0) {
+			this.speed = 1.0;
+		}
+		else if (this.speed < -1.0){
+			this.speed = -1.0;
+		}
+		/* Applies a deadband speed and rotation */
+		if (Math.abs(this.speed)< 0.1 || Math.abs(this.rotation) - this.rotationSetpoint < 5.0) {
+			return;
+		}
+		
+		double alpha = Math.pow(0.005, dt);
+		this.rotation = alpha * this.rotation + (1.0 - alpha) * this.rotationSetpoint;
+	}
 }
